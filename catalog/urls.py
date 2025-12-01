36cee1b272fe16d1d435223883e1fa7a75acb6bd
Andrei Tihomirov <moon200505@mail.ru>
2025-12-01 16:52:29 +0300

    done

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,4 @@ app_name= 'catalog'
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    # path('submit_data/', views.submit_data, name='submit_data'),
-    # path('item/<int:item_id>/', views.show_item, name='show_item')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
+    """Контролер рендеринга главной страницы"""
     return render(request, 'catalog/home.html')
 
 def contacts(request):
+    """Контролер рендеринга страницы контактов"""
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
         return HttpResponse(f'Уважаемый {name}, с номером {phone}. Ваше сообщение << {message} >> получено!')
     return render(request, "catalog/contacts.html")
-
-
-
-
-# def show_data(request):
-#     if request.method == 'GET':
-#         return render(request, 'app/data.html')
-#
-#
-# def submit_data(request):
-#     if request.method == 'POST':
-#         # Обработка данных формы
-#         return HttpResponse("Данные отправлены!")
-#
-#
-# def show_item(request, item_id):
-#     return render(request, 'app/item.html', {'item_id':item_id})
