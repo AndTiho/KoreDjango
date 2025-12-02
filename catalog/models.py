4a26f7b5d81b585faf88ae74c9f41211ce076165
Andrei Tihomirov <moon200505@mail.ru>
2025-12-02 17:21:20 +0300

    well...i forgot something ^^

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, unique=True, verbose_name='Наименование')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+    def __str__(self):
+        return f"{self.category_name} - {self.description}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = 'категории'
+        ordering = ['category_name']
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, unique=True, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    product_image = models.ImageField(upload_to='photos/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
+    price = models.FloatField(verbose_name="Цена за покупку")
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f"{self.product_name} - {self.description}. Цена {self.price}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = 'продукты'
+        ordering = ['product_name']
